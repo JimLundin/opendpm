1334a7b2d082dd86361b7c1ab6cdf1ca7dd7e6a4
Jim Lundin <jimeriklundin@gmail.com>
2025-01-20 19:17:34 +0100

    revert

--- src/dpm/convert.py
+++ src/dpm/convert.py
@@ -137,6 +137,5 @@ if __name__ == "__main__":
     project_root = Path(__file__).resolve().parents[3]
     input_dir = project_root / ".scratch" / "db_input"
     output_dir = project_root / ".scratch" / "db_output"
-    output_dir.mkdir(parents=True, exist_ok=True)
 
     process_access_files(input_dir, output_dir)
