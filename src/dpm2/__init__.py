e671f4f44cd4d2e424571d96c762500f4486228a
Jim Lundin <jimeriklundin@gmail.com>
2025-05-12 09:35:10 +0200

    feat: in memory copy of the db for queries

---
+++ src/dpm2/__init__.py
@@ -0,0 +1,7 @@
+"""DPM2 Data Package Module.
+
+This package contains data artifacts for DPM databases that are generated
+during the release process using conversion logic from the OpenDPM project.
+"""
+
+__version__ = "2025.5.7"
