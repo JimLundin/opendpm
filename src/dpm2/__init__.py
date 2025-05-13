b2fa8d6f5f32b8c88ff10377866d044ee134e09a
Jim Lundin <jimeriklundin@gmail.com>
2025-05-13 12:42:39 +0200

    dynamic version for data package

--- src/dpm2/__init__.py
+++ src/dpm2/__init__.py
@@ -4,4 +4,5 @@ This package contains data artifacts for DPM databases that are generated
 during the release process using conversion logic from the OpenDPM project.
 """
 
-__version__ = "2025.5.7"
+# The version is set by the build process
+__version__ = None
