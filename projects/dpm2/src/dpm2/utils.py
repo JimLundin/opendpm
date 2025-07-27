d99b1269208057f56814bd6342fa9def59a49c80
Jim Lundin <jimeriklundin@gmail.com>
2025-07-27 23:21:01 +0200

    dpm2: always read only on disk, allow mutation on in-memory

--- projects/dpm2/src/dpm2/utils.py
+++ projects/dpm2/src/dpm2/utils.py
@@ -7,7 +7,7 @@ from sqlalchemy import Connection, Engine, create_engine, text
 from sqlalchemy.event import listen
 from sqlalchemy.pool import ConnectionPoolEntry
 
-db_path = Path(__file__).parent / "dpm.sqlite"
+SOURCE_DB = Path(__file__).parent / "dpm.sqlite"
 
 
 def set_readonly(connection: Connection, _record: ConnectionPoolEntry) -> None:
@@ -17,19 +17,19 @@ def set_readonly(connection: Connection, _record: ConnectionPoolEntry) -> None:
 
 def disk_engine() -> Engine:
     """Get an engine to the dpm db."""
-    return create_engine(f"sqlite:///{db_path}?mode=ro", connect_args={"uri": True})
+    engine = create_engine(f"sqlite:///{SOURCE_DB}?mode=ro", connect_args={"uri": True})
+    listen(engine, "connect", set_readonly)
+    return engine
 
 
 def in_memory_engine() -> Engine:
     """Get an engine to the dpm db."""
     memory_db = connect(":memory:")
-    with connect(db_path) as source_db:
+    with connect(SOURCE_DB) as source_db:
         source_db.backup(memory_db)
     return create_engine("sqlite://", creator=lambda: memory_db)
 
 
 def get_db(*, in_memory: bool = False) -> Engine:
     """Get an engine to the dpm db."""
-    engine = in_memory_engine() if in_memory else disk_engine()
-    listen(engine, "connect", set_readonly)
-    return engine
+    return in_memory_engine() if in_memory else disk_engine()
