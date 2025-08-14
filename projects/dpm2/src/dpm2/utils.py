ad8364156a2c10d6af16dd9ada30ae977206a8cc
Jim Lundin <jimeriklundin@gmail.com>
2025-08-14 10:16:57 +0200

    fix(dpm2): fix publishing and use importlib for fetching files in distribution

--- projects/dpm2/src/dpm2/utils.py
+++ projects/dpm2/src/dpm2/utils.py
@@ -1,13 +1,18 @@
 """db utilities for using the dpm db."""
 
-from pathlib import Path
+from importlib.resources import as_file, files
+from importlib.resources.abc import Traversable
 from sqlite3 import connect
 
 from sqlalchemy import Connection, Engine, create_engine, text
 from sqlalchemy.event import listen
 from sqlalchemy.pool import ConnectionPoolEntry
 
-SOURCE_DB = Path(__file__).parent / "dpm.sqlite"
+
+def get_source_db_resource() -> Traversable:
+    """Get the path to the bundled SQLite database."""
+    package_files = files("dpm2")
+    return package_files / "dpm.sqlite"
 
 
 def set_readonly(connection: Connection, _record: ConnectionPoolEntry) -> None:
@@ -17,15 +22,24 @@ def set_readonly(connection: Connection, _record: ConnectionPoolEntry) -> None:
 
 def disk_engine() -> Engine:
     """Get an engine to the dpm db."""
-    engine = create_engine(f"sqlite:///{SOURCE_DB}?mode=ro", connect_args={"uri": True})
+    source_resource = get_source_db_resource()
+    with as_file(source_resource) as source_path:
+        engine = create_engine(
+            f"sqlite:///{source_path}?mode=ro",
+            connect_args={"uri": True},
+        )
     listen(engine, "connect", set_readonly)
     return engine
 
 
 def in_memory_engine() -> Engine:
     """Get an engine to the dpm db."""
+    source_resource = get_source_db_resource()
     memory_db = connect(":memory:")
-    with connect(SOURCE_DB) as source_db:
+    with (
+        as_file(source_resource) as source_path,
+        connect(source_path) as source_db,
+    ):
         source_db.backup(memory_db)
     return create_engine("sqlite://", creator=lambda: memory_db)
 
